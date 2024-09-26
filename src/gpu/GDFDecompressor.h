1b3a76bdbb1b438cc000174ed0f206afd58379e3
lz <1519316637@qq.com>
2024-09-26 15:23:06 +0800

    init

---
+++ src/gpu/GDFDecompressor.h
@@ -0,0 +1,16 @@
+//
+// Created by lz on 24-9-26.
+//
+
+#ifndef GDFDECOMPRESSOR_H
+#define GDFDECOMPRESSOR_H
+
+
+
+class GDFDecompressor {
+
+};
+
+
+
+#endif //GDFDECOMPRESSOR_H
---
+++ test/test_compression.h
@@ -0,0 +1,16 @@
+//
+// Created by lz on 24-9-26.
+//
+
+#ifndef TEST_COMPRESSION_H
+#define TEST_COMPRESSION_H
+
+
+
+class test_compression {
+
+};
+
+
+
+#endif //TEST_COMPRESSION_H
