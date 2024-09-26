7e6cec9cbb295f7b50f58cc6c39f70ef0772ff8b
lz <1519316637@qq.com>
2024-09-26 15:23:06 +0800

    init

---
+++ src/cpu/CDFCompressor.h
@@ -0,0 +1,16 @@
+//
+// Created by lz on 24-9-26.
+//
+
+#ifndef CDFCOMPRESSOR_H
+#define CDFCOMPRESSOR_H
+
+
+
+class CDFCompressor {
+
+};
+
+
+
+#endif //CDFCOMPRESSOR_H
---
+++ src/cpu/CDFDecompressor.h
@@ -0,0 +1,16 @@
+//
+// Created by lz on 24-9-26.
+//
+
+#ifndef CDFDECOMPRESSOR_H
+#define CDFDECOMPRESSOR_H
+
+
+
+class CDFDecompressor {
+
+};
+
+
+
+#endif //CDFDECOMPRESSOR_H
