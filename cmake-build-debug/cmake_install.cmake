7f09474e587c99445c025a134f855cd50cc829e5
lz <1519316637@qq.com>
2024-09-26 15:43:18 +0800

    init

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -52,6 +52,16 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("/home/lz/workspace/cuCompressor/cmake-build-debug/Serf/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/lz/workspace/cuCompressor/cmake-build-debug/src/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/lz/workspace/cuCompressor/cmake-build-debug/test/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
