748c8c01c85e8a9f8bd8152f9956134473e412f5
willinghear <948682907@qq.com>
2024-12-10 19:26:37 +0800

    gpu 1.01

--- include/GDFCompressor.cuh
+++ include/GDFCompressor.cuh
@@ -12,11 +12,8 @@
 #include <cstring>
 
 // 定义常量
-#define BLOCK_SIZE_G 1024
+#define BLOCK_SIZE_G 16
 #define POW_NUM_G ((1L << 51) + (1L << 52))
-
-
-__global__ void compressBlockKernel(const double* input, int blockSize, int totalSize, unsigned char* output, unsigned long* bitSizes);
 
 class GDFCompressor {
 public:
@@ -27,7 +24,8 @@ public:
 private:
     int blockSize;
 
-    void setupDeviceMemory(const std::vector<double>& input, double*& d_input, unsigned char*& d_output, unsigned long*& d_bitSizes);
+    void setupDeviceMemory(const std::vector<double>& input, double*& d_input, unsigned char*& d_output, uint64_t*& d_bitSizes);
 
-    void freeDeviceMemory(double* d_input, unsigned char* d_output, unsigned long* d_bitSizes);
+    void freeDeviceMemory(double* d_input, unsigned char* d_output, uint64_t* d_bitSizes);
+
 };
