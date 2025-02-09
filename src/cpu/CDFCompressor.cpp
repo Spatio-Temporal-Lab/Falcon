8f86a08fac2cdaa290ee19600ae89af6bbafef99
willinghear <948682907@qq.com>
2025-02-10 00:17:46 +0800

    test_cuSZp f64

--- src/cpu/CDFCompressor.cpp
+++ src/cpu/CDFCompressor.cpp
@@ -175,18 +175,23 @@ void CDFCompressor::compressBlock(const std::vector<double>& block, OutputBitStr
     for (int i = 0; i < flagArraySize; i++)
     {
         bitStream.WriteByte(flag[i]);
-
+    }
+    
     for (int i = 0; i < sparseTransposed.size(); i++)
     {
         if (sparseTransposed[i] != 0)
         {
             bitStream.WriteByte(sparseTransposed[i]);
 
+        }
+    }
+
     for (int i = 0; i < nonSparseSize; i++)
     {
         bitStream.WriteLong(transposedNonSparse[i], 64);
 
-
+    }
+}
 
 void CDFCompressor::sampleBlock(const std::vector<double>& block, std::vector<long>& longs, long& firstValue,
                                 int& maxDecimalPlaces, int& isOk)
@@ -196,7 +201,7 @@ void CDFCompressor::sampleBlock(const std::vector<double>& block, std::vector<lo
     int maxBeta = 0;
     for (double val : block)
     {
-//计算起始位置sp
+        //计算起始位置sp
         double log10v = log10(std::abs(val));
         int sp = floor(log10v);
         // 计算当前值的小数点后位数
