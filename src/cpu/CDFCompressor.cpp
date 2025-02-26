32b033aa4878c8a271961c87a2c4feb5fb7cca7a
willinghear <948682907@qq.com>
2025-02-26 15:12:57 +0800

    debug cpu

--- src/cpu/CDFCompressor.cpp
+++ src/cpu/CDFCompressor.cpp
@@ -175,19 +175,18 @@ void CDFCompressor::compressBlock(const std::vector<double>& block, OutputBitStr
     for (int i = 0; i < flagArraySize; i++)
     {
         bitStream.WriteByte(flag[i]);
-
-        for (int i = 0; i < sparseTransposed.size(); i++)
+    }
+    for (int i = 0; i < sparseTransposed.size(); i++)
+    {
+        if (sparseTransposed[i] != 0)
         {
-            if (sparseTransposed[i] != 0)
-            {
-                bitStream.WriteByte(sparseTransposed[i]);
-            }
+            bitStream.WriteByte(sparseTransposed[i]);
         }
+    }
 
-        for (int i = 0; i < nonSparseSize; i++)
-        {
-            bitStream.WriteLong(transposedNonSparse[i], 64);
-        }
+    for (int i = 0; i < nonSparseSize; i++)
+    {
+        bitStream.WriteLong(transposedNonSparse[i], 64);
     }
 }
 
