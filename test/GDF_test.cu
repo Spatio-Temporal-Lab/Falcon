39b6bf30f8d77e3efc66cf2effa4b19d27b6572f
willinghear <948682907@qq.com>
2025-03-16 22:01:34 +0800

    debug git

--- test/GDF_test.cu
+++ test/GDF_test.cu
@@ -20,45 +20,49 @@ namespace fs = std::filesystem;
 #include <algorithm>
 
 
-// 测试压缩和解压缩
-void test_compression(const std::string& file_path) {
-    // 读取数据
-    std::vector<double> oriData = read_data(file_path);
-    //std::cout<<" ordata : "<<oriData[0]<<std::endl;
-    // 压缩相关变量
-    std::vector<unsigned char> cmpData;
-    std::vector<unsigned int> cmpOffset;
-    std::vector<int> flag;
+
+
+void comp(std::vector<double> oriData,std::vector<double> &decompData)
+{
+    std::vector<unsigned char> cmpData1;//cpu
+    std::vector<unsigned char> cmpData2;//gpu
     size_t nbEle = oriData.size();
 
     // 记录压缩时间
-    std::cout<<"压缩开始\n";
+    std::cout<<"CPU压缩开始\n";
     auto start_compress = std::chrono::high_resolution_clock::now();
     //进行压缩
-    GDFCompressor GDFC;
-    GDFC.compress(oriData,cmpData);
+    CDFCompressor CDFC;
+    CDFC.compress(oriData,cmpData1);
+
     auto end_compress = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> compress_duration = end_compress - start_compress;
     // 打印压缩时间
-    std::cout << "压缩时间: " << compress_duration.count() << " 秒" << std::endl;
-    
-    // 解压缩相关变量
-    std::vector<double> decompressedData;
-    
-    // std::cout << "压缩后的数据内容: ";
-    // for (const auto& byte : cmpData) {
-    //     std::cout << std::hex << static_cast<int>(byte) << " "; // 打印为十六进制
-    // }
-    // std::cout << std::dec << std::endl; // 恢复为十进制格式
-    // std::cout << std::endl;
-    // }
-    // 记录解压时间
+    std::cout << "CPU压缩时间: " << compress_duration.count() << " 秒" << std::endl;
+
+
+    // 记录压缩时间
+    std::cout<<"GPU压缩开始\n";
+    start_compress = std::chrono::high_resolution_clock::now();
+    GDFCompressor GDFC;
+    GDFC.compress(oriData,cmpData2);
+    end_compress = std::chrono::high_resolution_clock::now();
+    compress_duration = end_compress - start_compress;
+
+    // 打印压缩时间
+    std::cout << "GPU压缩时间: " << compress_duration.count() << " 秒" << std::endl;
+
+
+
+        // 记录解压时间
     std::cout<<"解压开始\n";
     auto start_decompress = std::chrono::high_resolution_clock::now();
 
-    //进行解压
-    GDFDecompressor GDFD;
-    GDFD.decompress(cmpData,decompressedData,nbEle);
+    // 比较压缩后的数据
+    GDFDecompressor0 GDFD;//cpu解压
+    // GDFDecompressor GDFD;//gpu解压
+    GDFD.decompress(cmpData2, decompData,nbEle);
+
     auto end_decompress = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> decompress_duration = end_decompress - start_decompress;
 
@@ -67,114 +71,20 @@ void test_compression(const std::string& file_path) {
 
     // 计算压缩率
     size_t original_size = oriData.size() * sizeof(double);
-    size_t compressed_size = cmpData.size() * sizeof(unsigned char);
+    size_t compressed_size = cmpData2.size() * sizeof(unsigned char);
     double compression_ratio = compressed_size/ static_cast<double>(original_size);
 
     // 打印压缩率
     std::cout << "压缩率: " << compression_ratio << std::endl;
-    ASSERT_EQ(decompressedData.size() , oriData.size()) << "解压失败，数据不一致。";
-    for(int i=0;i<oriData.size();i++)
-    {
-        // 验证解压结果是否与原始数据一致
-        // std::cout << std::fixed << std::setprecision(10)<<decompressedData[i]<<" "<<oriData[i]<<std::endl;
-        ASSERT_EQ(decompressedData[i] , oriData[i]) <<i<< "解压失败，数据不一致。";
 
-    }
+    //ASSERT_EQ(decompData.size() , oriData.size()) << "解压失败，数据不一致。";
+    // for(int i=0;i<oriData.size();i++)
+    // {
+    //     // 验证解压结果是否与原始数据一致
+    //     //std::cout << std::fixed << std::setprecision(10)<<decompData[i]<<" "<<oriData[i]<<std::endl;
+    //     ASSERT_EQ(decompData[i] , oriData[i]) <<i<< "解压失败，数据不一致。";
 
-}
-
-
-// 测试压缩
-void test_compression0(const std::string& file_path) {
-    // 读取数据
-    std::vector<double> oriData = read_data(file_path);
-    std::vector<unsigned char> cmpData1;
-    std::vector<unsigned char> cmpData2;
-    std::vector<unsigned int> cmpOffset;
-    std::vector<int> flag;
-    size_t nbEle = oriData.size();
-
-    // 记录压缩时间
-    std::cout<<"CPU压缩开始\n";
-    auto start_compress = std::chrono::high_resolution_clock::now();
-    //进行压缩
-    CDFCompressor CDFC;
-    CDFC.compress(oriData,cmpData1);
-
-    auto end_compress = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> compress_duration = end_compress - start_compress;
-    // 打印压缩时间
-    std::cout << "CPU压缩时间: " << compress_duration.count() << " 秒" << std::endl;
-
-
-    // 记录压缩时间
-    std::cout<<"GPU压缩开始\n";
-    start_compress = std::chrono::high_resolution_clock::now();
-    GDFCompressor GDFC;
-    GDFC.compress(oriData,cmpData2);
-    end_compress = std::chrono::high_resolution_clock::now();
-    compress_duration = end_compress - start_compress;
-
-    // 打印压缩时间
-    std::cout << "GPU压缩时间: " << compress_duration.count() << " 秒" << std::endl;
-
-
-    // 比较压缩后的数据
-    try {
-        ASSERT_EQ(cmpData1.size(), cmpData2.size()) << "压缩后数据的大小不一致。";
-
-        for (size_t i = 0; i < cmpData1.size(); ++i) {
-            if (cmpData1[i] != cmpData2[i]) {
-                std::cerr << "数据不一致，位置: " << i << ", CPU 数据: " 
-                          << static_cast<int>(cmpData1[i]) 
-                          << ", GPU 数据: " 
-                          << static_cast<int>(cmpData2[i]) << std::endl;
-            }
-        }
-    } catch (const std::exception& e) {
-        std::cerr << "断言失败: " << e.what() << std::endl;
-    }
-    
-}
-
-
-
-void comp(std::vector<double> oriData,std::vector<double> &decompData)
-{
-    std::vector<unsigned char> cmpData1;
-    std::vector<unsigned char> cmpData2;
-    std::vector<unsigned int> cmpOffset;
-    std::vector<int> flag;
-    size_t nbEle = oriData.size();
-
-    // 记录压缩时间
-    std::cout<<"CPU压缩开始\n";
-    auto start_compress = std::chrono::high_resolution_clock::now();
-    //进行压缩
-    CDFCompressor CDFC;
-    CDFC.compress(oriData,cmpData1);
-
-    auto end_compress = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> compress_duration = end_compress - start_compress;
-    // 打印压缩时间
-    std::cout << "CPU压缩时间: " << compress_duration.count() << " 秒" << std::endl;
-
-
-    // 记录压缩时间
-    std::cout<<"GPU压缩开始\n";
-    start_compress = std::chrono::high_resolution_clock::now();
-    GDFCompressor GDFC;
-    GDFC.compress(oriData,cmpData2);
-    end_compress = std::chrono::high_resolution_clock::now();
-    compress_duration = end_compress - start_compress;
-
-    // 打印压缩时间
-    std::cout << "GPU压缩时间: " << compress_duration.count() << " 秒" << std::endl;
-
-
-    // 比较压缩后的数据
-    GDFDecompressor GDFD;
-    GDFD.decompress(cmpData2, decompData,nbEle);
+    // }
 
     // 打印压缩数据
     // std::cout << "\nCPU压缩结果:\n";
@@ -203,33 +113,19 @@ void comp(std::vector<double> oriData,std::vector<double> &decompData)
     }
 }
 
+
+// 测试压缩和解压缩
+void test_compression(const std::string& file_path) {
+    // 读取数据
+    std::vector<double> oriData = read_data(file_path);
+    std::vector<double> decompressedData;
+    comp(oriData,decompressedData);
+
+}
+
+
 // 测试压缩
 TEST(GDFCompressorTest0, CompressionDecompression) {
-    // 读取数据
-    //std::vector<double> oriData = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.8};//{0.001,0.122,0.01543,0.14,0.5,0.47};
-    const size_t size = 1024*256;
-    std::vector<double> oriData(size*2);
-
-    // 初始化数组，每个值递增 0.1
-    double startValue = 0.1;
-    double step = 0.1;
-
-    for (size_t i = 0; i < size; ++i) {
-        oriData[i] = startValue + i * step;
-    }
-    for (size_t i = size; i < size * 2; ++i) {
-        oriData[i] = std::floor((oriData[i - 1] - 0.05) * 100000000.0) / 100000000.0;
-    }
-
-
-    std::vector<double> newData;
-    comp(oriData,newData);
-    // for(auto n:newData)
-    // {
-    //     printf("%f,",n);
-    // }
-}
-TEST(GDFCompressorTest1, CompressionDecompression) {
     // 读取数据
 //    {0.1,0.2,0.3,0.4,0.5,0.12,0.1,0.3,2.1};//{0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7,0.8};//
     std::vector<double> oriData = {0.1, 0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.10,0.11,0.12,0.13,0.14,0.15,0.16,0.17,0.18,0.19,0.20,0.21,0.22,0.23,0.24,0.25,0.26,0.27,0.28,0.29,0.30,0.31,0.32,0.33};//{0.1, 0.2, 0.3, 0.4, 0.5, 0.1, 0.4, 0.2, 0.4, 0.1, 0.015, 0.23};
@@ -242,10 +138,33 @@ TEST(GDFCompressorTest1, CompressionDecompression) {
     printf("\n");
 }
 
+TEST(GDFCompressorTest1, CompressionDecompression) {
+    // 读取数据
+    //std::vector<double> oriData = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.8};//{0.001,0.122,0.01543,0.14,0.5,0.47};
+    const size_t size = 1024*256;
+    std::vector<double> oriData(size*2);
+
+    // 初始化数组，每个值递增 0.1
+    double startValue = 0.0001;
+    double step = 0.0000001;
+
+    for (size_t i = 0; i < size; ++i) {
+        oriData[i] = startValue + i * step;
+    }
+    for (size_t i = size; i < size * 2; ++i) {
+        oriData[i] = std::floor((oriData[i - 1] - 0.05) * 100000000.0) / 100000000.0;
+    }
+
+
+    std::vector<double> newData;
+    comp(oriData,newData);
+
+}
+
 std::vector<uint8_t> ConvertArrayToVector(const Array<uint8_t>& arr) {
     return std::vector<uint8_t>(arr.begin(), arr.end());
 }
-//Google Test 测试用例
+// //Google Test 测试用例
 TEST(CDFCompressorTest, CompressionDecompression) {
     std::string dir_path = "../test/data/float";//有毛病还没有数据集
     for (const auto& entry : fs::directory_iterator(dir_path)) {
--- test/test_ndzip.cpp
+++ test/test_ndzip.cpp
@@ -1,4 +1,4 @@
-/*cpu版本   
+//cpu版本   
 
     // #include "ndzip/ndzip.hh"
     // #include <gtest/gtest.h>
@@ -32,6 +32,8 @@
     //     // 读取数据
     //     std::vector<float> oriData = read_data(file_path);
 
+    //     // 压缩率相关变量
+    //     size_t original_size = oriData.size() * sizeof(float);
     //     // 压缩相关变量
     //     auto compressor = ndzip::make_compressor<float>(1); // 一维压缩器
     //     size_t compressedSize = ndzip::compressed_length_bound<float>({static_cast<ndzip::index_type>(oriData.size())});
@@ -43,11 +45,15 @@
     //     size_t actualCompressedSize = compressor->compress(oriData.data(), 
     //         {static_cast<ndzip::index_type>(oriData.size())}, cmpData.data());
     //     auto end_compress = std::chrono::high_resolution_clock::now();
-    //     cmpData.resize(actualCompressedSize);  // 调整压缩数据大小
+    //     cmpData.resize(actualCompressedSize/sizeof(ndzip::compressed_type<float>));  // 调整压缩数据大小
+        
     //     std::chrono::duration<double> compress_duration = end_compress - start_compress;
 
     //     // 输出压缩时间
     //     std::cout << "压缩时间: " << compress_duration.count() << " 秒\n";
+    //     // 在压缩时间输出后添加：
+    //     double compress_throughput = (original_size / (1024.0 * 1024.0)) / compress_duration.count();
+    //     std::cout << "压缩吞吐量: " << compress_throughput << " MB/s\n       " << compress_throughput/1024 <<" GB/S\n";
 
     //     // 解压缩相关变量
     //     auto decompressor = ndzip::make_decompressor<float>(1); // 一维解压缩器
@@ -63,10 +69,15 @@
 
     //     // 输出解压时间
     //     std::cout << "解压时间: " << decompress_duration.count() << " 秒\n";
-
+    //     // 在解压时间输出后添加：
+    //     double decompress_throughput = (original_size / (1024.0 * 1024.0)) / decompress_duration.count();
+    //     std::cout << "解压吞吐量: " << decompress_throughput << " MB/s\n       " << decompress_throughput/1024 <<" GB/S\n";
     //     // 计算压缩率
-    //     size_t original_size = oriData.size() * sizeof(float);
-    //     size_t compressed_size = cmpData.size() * sizeof(ndzip::compressed_type<float>);
+
+    //     // 直接使用字节数计算压缩率
+    //     // size_t compressed_size = actualCompressedSize;  // 这里已经是以字节为单位
+
+    //     size_t compressed_size = cmpData.size()*sizeof(uint32_t) ;//* sizeof(ndzip::compressed_type<float>);
     //     double compression_ratio = static_cast<double>(compressed_size) / original_size;
 
     //     // 输出压缩率
@@ -75,6 +86,10 @@
     //     // 验证解压结果
     //     ASSERT_EQ(decompressedData.size(), oriData.size()) << "解压失败，数据大小不一致";
     //     for (size_t i = 0; i < oriData.size(); ++i) {
+    //         if(i<5)
+    //         {
+    //             std::cout<<i<<" "<<decompressedData[i]<<" "<<oriData[i]<<"\n";
+    //         }
     //         ASSERT_FLOAT_EQ(decompressedData[i], oriData[i]) << i << " 解压数据与原始数据不一致";
     //     }
     // }
@@ -91,7 +106,7 @@
     //         }
     //     }
     // }
-*/
+//
 
 #include "ndzip/cuda.hh"
 #include <gtest/gtest.h>
@@ -129,7 +144,13 @@ void test_cuda_compression(const std::string &file_path) {
     // 数据大小
     size_t data_size = oriData.size();
     ndzip::extent data_extent{static_cast<ndzip::index_type>(data_size)};
-
+    
+    // 计算压缩率
+    size_t original_size = oriData.size() * sizeof(float);
+    // 开始压缩
+    std::cout << "CUDA 压缩开始\n";
+    auto start_compress = std::chrono::high_resolution_clock::now();
+    
     // 分配GPU内存
     float *device_data;
     cudaMalloc(&device_data, data_size * sizeof(float));
@@ -140,30 +161,54 @@ void test_cuda_compression(const std::string &file_path) {
         ndzip::compressor_requirements{data_extent});
 
     // 分配压缩结果存储的GPU内存
-    size_t compressed_size = ndzip::compressed_length_bound<float>(data_extent);
+    size_t compressed_size = ndzip::compressed_length_bound<float>(data_extent);//最大数据量
     ndzip::compressed_type<float> *device_compressed_data;
     cudaMalloc(&device_compressed_data, compressed_size * sizeof(ndzip::compressed_type<float>));
 
     ndzip::index_type *device_compressed_length;
     cudaMalloc(&device_compressed_length, sizeof(ndzip::index_type));
 
-    // 开始压缩
-    std::cout << "CUDA 压缩开始\n";
-    auto start_compress = std::chrono::high_resolution_clock::now();
+    // // 开始压缩
+    // std::cout << "CUDA 压缩开始\n";
+    // auto start_compress = std::chrono::high_resolution_clock::now();
     compressor->compress(device_data, data_extent, device_compressed_data, device_compressed_length);
-    auto end_compress = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> compress_duration = end_compress - start_compress;
-
+    // auto end_compress = std::chrono::high_resolution_clock::now();
+    // std::chrono::duration<double> compress_duration = end_compress - start_compress;
+    
     // 拷贝压缩结果到主机
-    ndzip::index_type host_compressed_length;
+    ndzip::index_type host_compressed_length;//压缩数据大小
     cudaMemcpy(&host_compressed_length, device_compressed_length, sizeof(ndzip::index_type), cudaMemcpyDeviceToHost);
-    std::vector<ndzip::compressed_type<float>> host_compressed_data(host_compressed_length);
+
+    std::vector<ndzip::compressed_type<float>> host_compressed_data(host_compressed_length);//压缩数据
     cudaMemcpy(host_compressed_data.data(), device_compressed_data,
                host_compressed_length * sizeof(ndzip::compressed_type<float>), cudaMemcpyDeviceToHost);
 
+    auto end_compress = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> compress_duration = end_compress - start_compress;
+
     // 输出压缩时间
     std::cout << "压缩时间: " << compress_duration.count() << " 秒\n";
-    std::cout << "实际压缩大小: " << host_compressed_length << " 元素\n";
+    double compress_throughput = (original_size / (1024.0 * 1024.0)) / compress_duration.count();
+    std::cout << "解压吞吐量: " << compress_throughput << " MB/s\n       " << compress_throughput/1024 <<" GB/S\n";
+    
+    // std::cout << "压缩流总元素数: " << host_compressed_length 
+    //       << " 元素大小: " << sizeof(ndzip::compressed_type<float>) << "字节\n";
+
+    //  std::cout << "原始数据大小: " 
+    //          << original_size << " bytes ("
+    //          << std::fixed << std::setprecision(2) 
+    //          << original_size / (1024.0 * 1024.0) << " MB / "
+    //          << original_size / (1024.0 * 1024.0 * 1024.0) << " GB)\n";
+    // static_assert(
+    //     sizeof(ndzip::compressed_type<float>) == 4,
+    //     "compressed_type<float>应为4字节"
+    // );
+    // 计算压缩率
+    size_t compressed_size_out = host_compressed_length * sizeof(ndzip::compressed_type<float>);
+    double compression_ratio = static_cast<double>(compressed_size_out) / original_size;
+
+    // 输出压缩率
+    std::cout << "压缩率: " << compression_ratio << "\n";
 
     // 创建CUDA解压缩器
     auto decompressor = ndzip::make_cuda_decompressor<float>(1);
@@ -186,7 +231,9 @@ void test_cuda_compression(const std::string &file_path) {
 
     // 输出解压时间
     std::cout << "解压时间: " << decompress_duration.count() << " 秒\n";
-
+    double decompress_throughput = (original_size / (1024.0 * 1024.0)) / decompress_duration.count();
+    std::cout << "解压吞吐量: " << decompress_throughput << " MB/s\n       " << decompress_throughput/1024 <<" GB/S\n";
+    
     // 验证解压结果
     ASSERT_EQ(host_decompressed_data.size(), oriData.size()) << "解压失败，数据大小不一致";
     for (size_t i = 0; i < oriData.size(); ++i) {
